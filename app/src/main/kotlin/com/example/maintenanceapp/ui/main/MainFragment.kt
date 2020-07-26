59bdb489cfd42a309122663446a4af84ee8b0bd7
Tommy <1952659+tommyshem@users.noreply.github.com>
2020-07-26 17:47:03 +0100

    added click listeners to buttons

--- app/src/main/kotlin/com/example/maintenanceapp/ui/main/MainFragment.kt
+++ app/src/main/kotlin/com/example/maintenanceapp/ui/main/MainFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import com.example.maintenanceapp.R
 import com.example.maintenanceapp.databinding.FragmentMainMenuBinding
 
@@ -20,19 +21,41 @@ class MainFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        Log.d(logTag,"onCreateView called")
+        Log.d(logTag, "onCreateView called")
         // A binding class is generated for each layout file.
         // By default, the name of the class is based on the name of the layout file,
         // converting it to Pascal case and adding the Binding suffix to it e.g fragment_users becomes FragmentUsersBinding
         //val bind: FragmentUsersBinding = FragmentUsersBinding.bind(fragmentView)
-        val binding = DataBindingUtil.inflate<FragmentMainMenuBinding>(inflater,R.layout.fragment_main_menu,container,false)
+        val binding = DataBindingUtil.inflate<FragmentMainMenuBinding>(
+            inflater,
+            R.layout.fragment_main_menu,
+            container,
+            false
+        )
+
+        // buttons on click setup
+        binding.buttonWorkOrders.setOnClickListener { view ->
+            view.findNavController().navigate(R.id.action_mainFragment_to_nav_work_orders)
+        }
+        binding.buttonPmChecklists.setOnClickListener { view ->
+            view.findNavController().navigate((R.id.action_mainFragment_to_nav_pm_check_lists))
+        }
+        binding.buttonInventoryItems.setOnClickListener { view ->
+            view.findNavController().navigate((R.id.action_mainFragment_to_nav_inventory_items))
+        }
+        binding.buttonUsers.setOnClickListener { view ->
+            view.findNavController().navigate((R.id.action_mainFragment_to_nav_users))
+        }
+        binding.buttonSettings.setOnClickListener { view ->
+            view.findNavController().navigate((R.id.action_mainFragment_to_nav_settings))
+        }
 
         return binding.root
     }
 
     override fun onStart() {
         super.onStart()
-        Log.d(logTag,"onStart called")
+        Log.d(logTag, "onStart called")
     }
 
 }
